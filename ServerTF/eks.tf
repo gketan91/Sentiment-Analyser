304c52e28abb43001eb5adc96b93f7eba8845461
Ketan Gupta <gketan91.kg@gmail.com>
2024-05-24 03:02:47 +0530

    update

--- ServerTF/eks.tf
+++ ServerTF/eks.tf
@@ -36,7 +36,7 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    instance_types = ["t2.medium"]
+    instance_types = ["t2.micro"]
   }
 
   eks_managed_node_groups = {
