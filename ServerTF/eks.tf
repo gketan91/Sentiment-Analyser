c3517ba57b4825b99da4dad02b7684e587f3ca8a
unknown <gketan91.kg@gmail.com>
2024-05-25 23:58:57 +0530

    update

--- ServerTF/eks.tf
+++ ServerTF/eks.tf
@@ -34,9 +34,12 @@ module "eks" {
   subnet_ids               = ["subnet-01d7fef9db2aee366","subnet-0b08c43fa9ed19ed1","subnet-08880c5551fe2cae3"]
   control_plane_subnet_ids = ["subnet-01d7fef9db2aee366","subnet-0b08c43fa9ed19ed1","subnet-08880c5551fe2cae3"]
 
+
   # EKS Managed Node Group(s)
   eks_managed_node_group_defaults = {
-    instance_types = ["t2.micro"]
+    instance_types = ["t2.medium"]
+    vpc_id =  "vpc-09320ee98f724a391"
+    security_groups = ["My-CustomSG"]
   }
 
   eks_managed_node_groups = {
